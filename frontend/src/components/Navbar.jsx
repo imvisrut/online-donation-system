f68181fc2857f35a27bc36e46c86a45f7b8c74aa
BHUMIT439439 <75707629+BHUMIT439439@users.noreply.github.com>
2021-10-06 16:04:24 +0530

    Frontend Fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,81 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect, useSelector } from "react-redux";
+import { logoutUser } from "../actions/authActions";
+import {Navbar,Container,Nav} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
-const CustomNav = () => {
+const DummyView = ({onLogoutClick, _this}) => {
+  const isAuthenticated = useSelector(state => state.isAuthenticated)
+  useEffect(() => {
+      _this.setState({
+          reduxState : isAuthenticated
+      })
+  }, [])
   return (
-    <Navbar bg="dark" className="navbar-dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Donate</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Profile</Nav.Link>
-          </Nav>
-          <Nav className="ml-auto">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-};
+    <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto"></Nav>
 
-export default CustomNav;
+          {!isAuthenticated && (
+            <Nav className="ml-auto">
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </Nav>
+          )}
+          {isAuthenticated && (
+            <Nav className="ml-auto">
+              <Link className="nav-link" to="/profile">
+                Profile
+              </Link>
+              <button
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem",
+                }}
+                onClick={onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Logout
+              </button>
+            </Nav>
+          )}
+        </Navbar.Collapse>
+  )
+}
+
+class CustomNav extends Component {
+  onLogoutClick = (e) => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+  
+
+  render() {
+    const { user } = this.props.auth;
+    
+    return (
+      <Navbar bg="dark" className="navbar-dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Donate</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <DummyView onLogoutClick={this.onLogoutClick} _this = {this} />
+        </Container>
+      </Navbar>
+    );
+  }
+}
+CustomNav.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { logoutUser })(CustomNav);
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
+
 const middleware = [thunk];
 
 const store = createStore(
@@ -10,7 +11,10 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+      compose
   )
 );
+
 export default store;
